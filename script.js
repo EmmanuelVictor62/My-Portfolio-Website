ffc3231e8edd4e339a309d5791450562cbf2f32c
EmmanuelVictor62 <royalmanuel62@gmail.com>
2022-07-28 11:27:22 +0100

    Styled second layer

---
+++ script.js
@@ -0,0 +1,14 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script> 
+<script>
+    var typed = new Typed('.type', {
+    strings: 
+    [
+     `Hello World`, 
+      "Welcome to my Universe",
+    ],
+    typespeed:60,
+    backspeed:60,
+    loop:true,
+    showCursor: true,
+    
+  })  
